cb5199bae9695c2724747bc0b7e52bcfe49497ce
stan055 <stanislav055@gmail.com>
2021-01-15 17:48:21 +0200

    fix

--- js/draw_wave5.js
+++ js/draw_wave5.js
@@ -2,9 +2,8 @@
 // Drawing
 function drawWave5(){
 
-    function createArraysData() {
         wave5.analyser.getByteFrequencyData(wave5.array);
-    
+
         // Clear canvas
         wave5.ctx.clearRect(0, 0, wave5.width, wave5.height)
         const arrayHeightBars = [];
@@ -17,22 +16,19 @@ function drawWave5(){
             arrayHeightBars.push(barHeight);
             arraySteps.push(step);
         }
+        draw(arrayHeightBars, arraySteps);
 
-        draw(arrayHeightBars, arraySteps, wave5.height);
+        window.requestAnimationFrame(drawWave5);
 
-        window.requestAnimationFrame(createArraysData);
-
-    };
-    createArraysData();
 }
 
 
-function draw(arrayHeightBars, arraySteps, height) {
+function draw(arrayHeightBars, arraySteps) {
     for (let i = 0; i < arrayHeightBars.length; i++) {
-        wave5.ctx.strokeStyle = `hsl(${arrayHeightBars[i] / (height*5) * 600}, 75%, 55%)`;
+        wave5.ctx.strokeStyle = `hsl(${arrayHeightBars[i] / (wave5.height*5) * 600}, 75%, 55%)`;
         wave5.ctx.beginPath();
-        wave5.ctx.moveTo(arraySteps[i], height);
-        wave5.ctx.lineTo(arraySteps[i], height - arrayHeightBars[i]);
+        wave5.ctx.moveTo(arraySteps[i], wave5.height);
+        wave5.ctx.lineTo(arraySteps[i], wave5.height - arrayHeightBars[i]);
         wave5.ctx.stroke();
     }
 }
