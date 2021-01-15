470655e738d87d2a5fe4af7caa6384d6545a817e
stan055 <stanislav055@gmail.com>
2021-01-15 17:13:39 +0200

    fix

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,34 +1,32 @@
 
 var blob = window.URL || window.webkitURL;
-    if (!blob) {
-        console.log('Your browser does not support Blob URLs :(');
-                 
-    }
+if (!blob) {
+    console.log('Your browser does not support Blob URLs :(');
+             
+}
+
+// Global var 
+var wave5;
 
 document.getElementById('file').addEventListener('change', function(event){
 
-      console.log('change on input#file triggered');
-      var file = this.files[0],
-       fileURL = blob.createObjectURL(file);
-      console.log(file);
-      console.log('File name: '+file.name);
-      console.log('File type: '+file.type);
-      console.log('File BlobURL: '+ fileURL);
-      document.getElementById('audio').src = fileURL;
+    console.log('change on input#file triggered');
+    var file = this.files[0],
+     fileURL = blob.createObjectURL(file);
+    console.log(file);
+    console.log('File name: '+file.name);
+    console.log('File type: '+file.type);
+    console.log('File BlobURL: '+ fileURL);
+    document.getElementById('audio').src = fileURL;
 
-      preparation();
-      audio.play();
-
-});
-
-var wave5;
-
-function preparation(){
     const canvas = document.getElementById('visualizer')
     const audio = document.getElementById("audio");
     var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
     
     wave5 = new Wave5(audioCtx, audio, canvas, 45);
-    
     drawWave5();
-}
+
+    audio.play();
+
+});
+
