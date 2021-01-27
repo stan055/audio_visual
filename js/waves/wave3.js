390541503ab5562dba92ee59dc13bf5bf8f0e1d7
stan055 <stanislav055@gmail.com>
2021-01-27 16:00:10 +0200

    fix

--- js/waves/wave3.js
+++ js/waves/wave3.js
@@ -6,7 +6,7 @@ class Wave3 extends WaveSuperClass{
   minDecibels = -80;
   styles = [[1, 'hsl(1, 99%, 99%)']];
   lineWidth = 1;
-  factorRadiusOfCircles = [5.5, 3.8, 2];
+  factorRadiusOfCircles = [5.5, 3.8, 2]; // Difference factor radius of circles
 
   constructor(canvas) {
     super(canvas);
